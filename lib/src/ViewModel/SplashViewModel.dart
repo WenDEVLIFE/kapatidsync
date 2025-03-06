a20ad5e48777d7edb69c492aa84ac237491aa3b3
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-06 22:15:54 +0800

    Added a function where it will navigation to login 👌

--- lib/src/ViewModel/SplashViewModel.dart
+++ lib/src/ViewModel/SplashViewModel.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/cupertino.dart';
 
+import '../config/Route.dart';
+
 class SplashViewModel extends ChangeNotifier {
 
+   bool loading = false;
+
+  bool get isLoading => loading;
+
+  Future<void> setLoading(BuildContext context) async {
+    loading = true;
+    notifyListeners();
+
+    await Future.delayed(const Duration(seconds: 3));
+
+    Navigator.pushNamed(context, RouteUtil.loginScreen);
+  }
 }
--- lib/src/ui/SplashUI.dart
+++ lib/src/ui/SplashUI.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kapatidsync/src/ViewModel/SplashViewModel.dart';
+import 'package:provider/provider.dart';
 
 class SplashUI extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    final SplashViewModel splashViewModel = Provider.of<SplashViewModel>(context);
+
     return Scaffold(
-      body: Center(
-        child: Text('Splash Screen'),
-      ),
+      body: FutureBuilder(future: splashViewModel.setLoading(context),
+          builder: (context, snapshot){
+        return const Center(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+        );
+
+          }
+      )
     );
   }
 }
