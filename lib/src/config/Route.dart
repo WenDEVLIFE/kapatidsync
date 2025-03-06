d2ae76501f8f77004edbbcd89cba9bc7afee1c59
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-06 14:24:41 +0800

    Implement the Route.dart and added LoginViewModel.dart and UserRepository.dart 👌

--- lib/src/config/Route.dart
+++ lib/src/config/Route.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:kapatidsync/src/ui/LoginUI.dart';
 import 'package:kapatidsync/src/ui/SplashUI.dart';
 
 class RouteUtil {
 
   static const String splashScreen ='/splashScreen';
+  static const String loginScreen ='/loginScreen';
 
-  Map <String, WidgetBuilder> routes = {
+  static Map <String, WidgetBuilder> routes = {
     splashScreen: (context) => SplashUI(),
+    loginScreen: (context) => const LoginUi(),
   };
 }
