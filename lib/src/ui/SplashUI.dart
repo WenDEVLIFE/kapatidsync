5cdc0796915ebb65d2d8414030f998139b4e93ca
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-06 13:38:27 +0800

    Change to mmvm structure

--- lib/src/ui/SplashUI.dart
+++ lib/src/ui/SplashUI.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class SplashUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Text('Splash Screen'),
       ),
