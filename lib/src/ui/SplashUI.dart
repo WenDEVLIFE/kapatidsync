68fcd59721f23b0cf9e9b1c8d9786c41734979f7
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-07 14:45:08 +0800

    Added animations on the SplashUI.dart and fix the dialog on the AlertDialogOptionWidget.dart 👌

--- lib/src/ui/SplashUI.dart
+++ lib/src/ui/SplashUI.dart
@@ -6,56 +6,83 @@ import 'package:provider/provider.dart';
 
 import '../config/ImageUtils.dart';
 
-class SplashUI extends StatelessWidget {
+class SplashUI extends StatefulWidget {
+  @override
+  _SplashUIState createState() => _SplashUIState();
+}
+
+class _SplashUIState extends State<SplashUI> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<int> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<SplashViewModel>(context, listen: false).setLoading(context);
+    });
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat();
+
+    _animation = IntTween(begin: 0, end: 3).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final SplashViewModel splashViewModel = Provider.of<SplashViewModel>(context);
-
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       backgroundColor: ColorUtils.primaryColor,
-      body: FutureBuilder(future: splashViewModel.setLoading(context),
-          builder: (context, snapshot){
-        return Center(
-            child: Center(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      height: screenHeight * 0.3,
-                      width: screenWidth * 0.5,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(ImageUtils.logoPath),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+      body: Consumer<SplashViewModel>(
+        builder: (context, splashViewModel, child) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: screenHeight * 0.3,
+                  width: screenWidth * 0.5,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(ImageUtils.logoPath),
+                      fit: BoxFit.cover,
                     ),
-                    const CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                    ),
-                    SizedBox(height: screenHeight * 0.02),
-                    const Text(
-                      'Loading...',
-                      style: TextStyle(
+                  ),
+                ),
+                const CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
+                SizedBox(height: screenHeight * 0.02),
+                AnimatedBuilder(
+                  animation: _animation,
+                  builder: (context, child) {
+                    String dots = '.' * (_animation.value + 1);
+                    return Text(
+                      'Loading$dots',
+                      style: const TextStyle(
                         color: Colors.white,
                         fontSize: 20,
                         fontFamily: 'Lato',
                         fontWeight: FontWeight.w700,
                       ),
-                    ),
-                  ],
+                    );
+                  },
                 ),
-              )
+              ],
             ),
-        );
-
-          }
-      )
+          );
+        },
+      ),
     );
   }
 }
--- lib/src/widget/AlertDialogOptionWidget.dart
+++ lib/src/widget/AlertDialogOptionWidget.dart
@@ -10,49 +10,46 @@ class AlertDialogOptionWidget {
   AlertDialogOptionWidget({required this.title, required this.content, required this.onPressed});
 
   Widget build(BuildContext context) {
-    return Dialog(
+    return AlertDialog(
       backgroundColor: ColorUtils.primaryColor,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(title, style: const TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontFamily: 'Lato',
-            fontWeight: FontWeight.w700,
-          )),
-          Text(content, style: const TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontFamily: 'Lato',
-            fontWeight: FontWeight.w400,
-          )),
-          TextButton(
-            onPressed: onPressed,
-            child: const Text('Yes',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontFamily: 'Lato',
-                fontWeight: FontWeight.w700,
-              ),
+      title: Text(title, style: const TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontFamily: 'Lato',
+        fontWeight: FontWeight.w700,
+      )),
+      content: Text(content, style: const TextStyle(
+        color: Colors.white,
+        fontSize: 16,
+        fontFamily: 'Lato',
+        fontWeight: FontWeight.w400,
+      )),
+      actions: <Widget>[
+        TextButton(
+          onPressed: onPressed,
+          child: const Text('Yes',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontFamily: 'Lato',
+              fontWeight: FontWeight.w700,
             ),
           ),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text('No',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontFamily: 'Lato',
-                fontWeight: FontWeight.w700,
-              ),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('No',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontFamily: 'Lato',
+              fontWeight: FontWeight.w700,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
