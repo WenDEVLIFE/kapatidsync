a43b5f53bf252e3206f94d4a4d6ecd5b4ad20985
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-07 15:06:08 +0800

    Added FirebaseService.dart 👌

---
+++ lib/src/services/FirebaseService.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseServices {
+  static Future<void> runFirebase() async {
+    // Add your code here
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        appId: '1:320080664161:android:800f0ae38140db8ccabc20',
+        apiKey: 'AIzaSyCqLvro3Syjzcf5zA1XwE4ZpcaAI-5WF8g',
+        projectId: 'kapatidsyncdb',
+        messagingSenderId: '320080664161',
+        storageBucket: 'kapatidsyncdb.firebasestorage.app'
+      ),
+    );
+
+    if (Firebase.apps.isEmpty) {
+      await Firebase.initializeApp(
+        options: const FirebaseOptions(
+          appId: '1:320080664161:android:800f0ae38140db8ccabc20',
+          apiKey: 'AIzaSyCqLvro3Syjzcf5zA1XwE4ZpcaAI-5WF8g',
+          projectId: 'kapatidsyncdb',
+          messagingSenderId: '320080664161',
+          storageBucket: 'kapatidsyncdb.firebasestorage.app'
+        ),
+      );
+    }
+
+    else{
+      print('Firebase already running');
+    }
+  }
+}
