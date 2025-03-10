4d0fe3fb21962a4974c7fa356c2b42bf0b6515e1
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-10 13:23:53 +0800

    Added SessionUtils.dart 👌

---
+++ lib/src/config/SessionUtils.dart
@@ -0,0 +1,38 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SessionUtils {
+  Future<void> saveUserInfo(Map<String, dynamic> userInfo) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('email', userInfo['email']);
+    prefs.setString('role', userInfo['role']);
+    prefs.setString('name', userInfo['name']);
+    prefs.setString('uid', userInfo['uid']);
+    print('User Info Saved');
+  }
+
+  Future<Map<String, dynamic>?> getUserInfo() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey('email')) {
+      return {
+        'email': prefs.getString('email'),
+        'role': prefs.getString('role'),
+        'name': prefs.getString('name'),
+        'uid': prefs.getString('uid')
+      };
+    }
+    return null;
+  }
+
+  Future<void> updateEmail(String email) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('email', email);
+  }
+
+  Future<void> clearUserInfo() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('email');
+    prefs.remove('role');
+    prefs.remove('name');
+    prefs.remove('uid');
+  }
+}
