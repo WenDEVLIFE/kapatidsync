e4b0387a9feac943a287dd7e6517d2339c503377
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-10 14:43:47 +0800

    Added admin.png and officer.png to the ImageUtils.dart 👌

--- lib/src/config/ImageUtils.dart
+++ lib/src/config/ImageUtils.dart
@@ -1,3 +1,5 @@
 class ImageUtils {
   static const String logoPath  = 'assets/images/logo.png';
+  static const String adminPath = 'assets/images/admin.png';
+  static const String officerPath = 'assets/images/officer.png';
 }
