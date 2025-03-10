20c6ae586d1eaf8be21e1cfa1283672a45a7589e
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-10 16:43:02 +0800

    Added display the user data on the UserViewModel.dart and to the UserUI.dart 👌

--- lib/src/ViewModel/UserViewModel.dart
+++ lib/src/ViewModel/UserViewModel.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kapatidsync/src/Repositoryy/UserRepository.dart';
 import 'package:kapatidsync/src/config/CheckDataUtils.dart';
 import 'package:kapatidsync/src/widget/FlutterToastWidget.dart';
+
+import '../model/UserModel.dart';
 
 class UserViewModel extends ChangeNotifier {
 
@@ -10,6 +13,7 @@ class UserViewModel extends ChangeNotifier {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmPasswordController = TextEditingController();
+  final TextEditingController searchController = TextEditingController();
 
   final CheckDataUtils checkDataUtils = CheckDataUtils();
   final UserRepository userRepository = UserRepositoryImpl();
@@ -20,25 +24,48 @@ class UserViewModel extends ChangeNotifier {
   bool get isObscure1 => obscureText1;
   bool get isObscure2 => obscureText2;
 
+  List<UserModel> users = [];
+  List<UserModel> filterUsers = [];
+
+  // Get users
+  List<UserModel> get getUser => filterUsers;
+
+  Stream<List<UserModel>> get userStream => userRepository.getUsers();
+
   var role = ['Admin', 'Chruch Officer'];
   var selectedRole ='Admin';
 
+  // intialize
+  UserViewModel  () {
+    _listenToUserStream();
+  }
+
+  // view password & hide password
   void toggleObscureText1() {
     obscureText1 = !obscureText1;
     notifyListeners();
   }
 
+  // view password & hide password
   void toggleObscureText2() {
     obscureText2 = !obscureText2;
     notifyListeners();
   }
 
+  // Set users
+  void setUsers(List<UserModel> userList) {
+    users = userList;
+    filterUsers = userList;
+    notifyListeners();
+  }
 
+  // Set role
   void setRole(String? value) {
     selectedRole = value!;
     notifyListeners();
   }
 
+  // clear data
   void clearData(){
     emailController.clear();
     nameController.clear();
@@ -47,6 +74,7 @@ class UserViewModel extends ChangeNotifier {
     selectedRole = 'Admin';
   }
 
+  // Add user listener
   Future<void> addUser() async {
 
     bool checkEmail = await checkDataUtils.validateEmail(emailController.text);
@@ -114,6 +142,35 @@ class UserViewModel extends ChangeNotifier {
       await userRepository.addUser(userdata);
       clearData();
     }
+
+  }
+
+  // get user
+  void _listenToUserStream() {
+    userStream.listen((userList) {
+      setUsers(userList);
+      notifyListeners();
+    });
+  }
+
+
+  // Filtered Search
+  void filterUser(String query) {
+    if (query.isNotEmpty) {
+      filterUsers = users
+          .where((user) =>
+      user.name.toLowerCase().contains(query.toLowerCase()) ||
+          user.email.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    } else {
+      filterUsers = users;
+    }
+    notifyListeners();
+  }
+
+
+  // Delete user
+  Future <void> deleteUser(String uid) async {
 
   }
 
--- lib/src/ui/UserUI.dart
+++ lib/src/ui/UserUI.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kapatidsync/src/widget/AddUserDialog.dart';
+import 'package:provider/provider.dart';
 
+import '../ViewModel/UserViewModel.dart';
 import '../config/ColorUtils.dart';
+import '../model/UserModel.dart';
 
 class UserUI extends StatelessWidget {
   @override
@@ -24,43 +27,106 @@ class UserUI extends StatelessWidget {
       body: Column(
         children: [
           SizedBox(height: screenHeight * 0.005),
-          Container(
-            width: screenWidth * 0.99,
-            height: screenHeight * 0.08,
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.transparent, width: 7),
-            ),
-            child: TextField(
-             // controller: _subscriptionViewModel.searchController,
-              onChanged: (query) {
-             //   _subscriptionViewModel.filterSubscriptions(query);
-              },
-              decoration: InputDecoration(
-                filled: true,
-                prefixIcon: const Icon(Icons.search, color: Colors.black),
-                fillColor: Colors.grey[200],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: const BorderSide(color: Colors.transparent, width: 2),
+          Consumer<UserViewModel>(builder: (context, viewModel, child)  {
+            return Container(
+              width: screenWidth * 0.99,
+              height: screenHeight * 0.08,
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.transparent, width: 7),
+              ),
+              child: TextField(
+                controller: viewModel.searchController,
+                onChanged: (query) {
+                   viewModel.filterUser(query);
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  prefixIcon: const Icon(Icons.search, color: Colors.black),
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.transparent, width: 2),
+                  ),
+                  hintText: 'Search a user....',
+                  hintStyle: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 0),
                 ),
-                hintText: 'Search a user....',
-                hintStyle: const TextStyle(
+                style: const TextStyle(
+                  fontFamily: 'LeagueSpartan',
                   color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
                 ),
-                contentPadding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 0),
               ),
-              style: const TextStyle(
-                fontFamily: 'LeagueSpartan',
-                color: Colors.black,
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              ),
+            );
+          }),
+          SizedBox(height: screenHeight * 0.005),
+          Expanded(
+            child: Consumer<UserViewModel>(
+              builder: (context, viewModel, child) {
+                return ListView.builder(
+                  itemCount: viewModel.getUser.length,
+                  itemBuilder: (context, index) {
+                    UserModel user = viewModel.getUser[index];
+
+                    return Card(
+                      color: ColorUtils.primaryColor,
+                      child: ListTile(
+                        title: Text(
+                          user.name,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800,
+                            fontFamily: 'SmoochSans',
+                            color: Colors.white,
+                          ),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Role: ${user.role}',
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w800,
+                                fontFamily: 'SmoochSans',
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              'Email: ${user.email}',
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w800,
+                                fontFamily: 'SmoochSans',
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.delete, color: Colors.white),
+                              onPressed: () {
+                                //viewModel.navigateToAddAdmin(context);
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
             ),
           ),
-          SizedBox(height: screenHeight * 0.005),
         ],
       ),
       floatingActionButton: FloatingActionButton(
