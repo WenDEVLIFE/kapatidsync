aedda59f7844a6670f9012c58887f2cc7409af9e
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-15 14:17:35 +0800

    Added KidAttendanceUI.dart and added to the Route.dart 👌

--- lib/src/config/Route.dart
+++ lib/src/config/Route.dart
@@ -3,15 +3,19 @@ import 'package:kapatidsync/src/ui/LoginUI.dart';
 import 'package:kapatidsync/src/ui/SplashUI.dart';
 import 'package:kapatidsync/src/widget/BottomNavigationWidget.dart';
 
+import '../ui/KidAttendanceUI.dart';
+
 class RouteUtil {
 
   static const String splashScreen ='/splashScreen';
   static const String loginScreen ='/loginScreen';
   static const String userPage ='/userPage';
+  static const String kidAttendance ='/kidAttendance';
 
   static Map <String, WidgetBuilder> routes = {
     splashScreen: (context) => SplashUI(),
     loginScreen: (context) => const LoginUi(),
     userPage: (context) => BottomNavigationWidget(),
+    kidAttendance: (context) => KidAttendanceUI(),
   };
 }
---
+++ lib/src/ui/KidAttendanceUI.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class KidAttendanceUI extends StatefulWidget {
+  @override
+  _KidAttendanceUIState createState() => _KidAttendanceUIState();
+}
+
+class _KidAttendanceUIState extends State<KidAttendanceUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
