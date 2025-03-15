ccd0928197318364586e80e82f93f1e397ca3925
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-03-15 13:26:56 +0800

    Added get the attendace list model on the database 👌

--- lib/src/Repositoryy/AttendanceRepository.dart
+++ lib/src/Repositoryy/AttendanceRepository.dart
@@ -1,7 +1,18 @@
-abstract class AttendanceRepository {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:kapatidsync/src/model/AttendanceModel.dart';
 
+abstract class AttendanceRepository {
+ Stream<List<AttendanceModel>> getAttendance();
 }
 
 class AttendanceRepositoryImpl implements AttendanceRepository {
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
+  // Get the attendance list
+  @override
+  Stream<List<AttendanceModel>> getAttendance() {
+
+    return firebaseFirestore.collection('attendancecollection').snapshots().map((snapshot) => snapshot.docs.map((doc) => AttendanceModel.fromDocumentSnapShot(doc)).toList());
+  }
 
 }
